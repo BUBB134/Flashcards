9fefcee7310f89083c3554d5fb96826c32810881
BUBB134 <87833879+BUBB134@users.noreply.github.com>
2024-08-15 20:52:19 +0100

    Add files via upload

---
+++ app/flashcards/page.js
@@ -0,0 +1,49 @@
+//app/flashcards/page.js
+import { useRouter } from "next/router"
+
+
+export default function Flashcard() {
+    
+    const { isLoaded, isSignedIn, user } = useUser()
+    const [flashcards, setFlashcards] = useState([])
+    const router = useRouter()
+    
+    useEffect(() => {
+        async function getFlashcards() {
+            if (!user) return
+            const docRef = doc(collection(db, 'users'), user.id)
+            const docSnap = await getDoc(docRef)
+            if (docSnap.exists()) {
+                const collections = docSnap.data().flashcards || []
+                setFlashcards(collections)
+            } else {
+                await setDoc(docRef, { flashcards: [] })
+            }
+        }
+        getFlashcards()
+    }, [user])
+    
+    const handleCardClick = (id) => {
+        router.push(`/flashcard?id=${id}`)
+    }
+}
+
+return (
+    <Container maxWidth="md">
+        <Grid container spacing={3} sx={{ mt: 4 }}>
+            {flashcards.map((flashcard, index) => (
+                <Grid item xs={12} sm={6} md={4} key={index}>
+                    <Card>
+                        <CardActionArea onClick={() => handleCardClick(flashcard.name)}>
+                            <CardContent>
+                                <Typography variant="h5" component="div">
+                                    {flashcard.name}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+            ))}
+        </Grid>
+    </Container>
+)
